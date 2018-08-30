9c8765c868eea28ed20a7b96c37edd5dbb376c81
Tine Kondo <kondotine@gmail.com>
2018-08-30 07:15:54 +0200

    feat(storybook-schematic): add option `--tsconfig` to allow using a custom `tsconfig.json` file for Storybook

--- src/storybook/schema.d.ts
+++ src/storybook/schema.d.ts
@@ -11,4 +11,8 @@ export interface StorybookOptions {
      * Exclude your stories from being compiled when running your angular dev environment.
      */
     excludeStoriesFromAppCompilation?: boolean
+    /**
+     * Specifies whether or not a custom tsconfig.json file should be used.
+     */
+    tsconfig?: boolean
 }
