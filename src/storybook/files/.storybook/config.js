c2021c85b1d5d45966322c55627937a2bddf879a
Tine Kondo <kondotine@gmail.com>
2018-08-31 00:27:41 +0200

    feat(storybook-schematic): allow automatically loading all story files that end with `*.stories.ts`

--- src/storybook/files/.storybook/config.js
+++ src/storybook/files/.storybook/config.js
@@ -1,7 +1,12 @@
 import { configure } from '@storybook/angular';
 
 function loadStories() {
-  require('../src/stories/index.ts');
+  // stories here are top of the list so they are the first one displayed
+  require('../src/stories');
+
+  // automatically import all story ts files that end with *.stories.ts
+  const req = require.context('../src/stories', true, /\.stories\.ts$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
